2d9f5d8f92f347b39391c97f60cfa4b1d58e3df9
donghuan01 <donghuan01@zhihu.com>
2025-07-22 13:37:41 +0800

    fix: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "dh 的记录博客",
+  description: "分享学习、技术、生活",
+  theme: 'reco',
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '计算机基础', link: '/计算机基础/计算机基础' },
+    ],
+  },
+};
+
